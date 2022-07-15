6e2c7854d10337e2b8c867622320714f124cebd1
wang-pengyang-6 <1558851538@qq.com>
2022-07-15 15:24:12 +0800

    学习第一天学习的代码

---
+++ test_7.15/test.c
@@ -0,0 +1,49 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//int main()
+//{
+//	printf("Hello World");
+//	return 0;
+//}
+
+//大多数人学习打出来的第一行代码一定是 Hello World 嘿嘿
+
+//
+//int main()
+//{
+//	printf("%zd\n",sizeof(char));
+//	printf("%zd\n", sizeof(short));
+//	printf("%zd\n", sizeof(int));
+//	printf("%zd\n", sizeof(long));
+//	printf("%zd\n", sizeof(long long));
+//	printf("%zd\n", sizeof(float));
+//	printf("%zd\n", sizeof(double));
+//	return 0;
+//}
+// 
+// 1 2 4 4 8 4 8 单位都是字节
+
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	scanf("%d %d",&num1,&num2);
+//	printf("%d",num1+num2);
+//	return 0;
+//}
+
+//写的一个非常简单的加和程序
+//
+//int a = 100;
+//int main()
+//{
+//	int a = 10;
+//	printf("%d",a);
+//	return 0;
+//}
+
+//此程序输出结果是 10 ，可以证明在用到变量时候，如果变量的表述符号在全局和局部中都出现了，则采取就近的原则
+
+
