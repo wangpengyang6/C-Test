1554450c2addab2c0a1aa1de2a607c968980bcaf
wang-pengyang-6 <1558851538@qq.com>
2022-07-18 22:08:40 +0800

    这是2022年7.18 学习C语言的代码

---
+++ test_7_18/test.c
@@ -0,0 +1,105 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#define N 100
+//int MAX(int a, int b)
+//{
+//	if (a > b)
+//	{
+//		return a;
+//	}
+//	else
+//	{
+//		return b;
+//	}
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d",&a,&b);
+//	int z = MAX(a, b);
+//	printf("%d",z);
+//	return 0;
+//}
+// 
+//int main()
+//{
+//	printf("N£º%d", N);
+//	return 0;
+//}
+
+//enum SEX
+//{
+//    MALE,
+//    FEMALE,
+//    UNKNOWN
+//};
+//int main()
+//{
+//    printf("%d\n", MALE);
+//    printf("%d\n", FEMALE);
+//    printf("%d\n", UNKNOWN);
+//    return 0;
+//}
+// 
+// 
+//#include<stdio.h>
+//int main()
+//{
+//    int n = 0;
+//    int time = 2;
+//    scanf("%d", &n);
+//    if (n < 12)
+//        time = 2;
+//    else
+//        time += (n / 12) * 4;
+//
+//    printf("%d", time);
+//    return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//    int n = printf("Hello world!");
+//    printf("\n%d", n);
+//    return 0;
+//}
+//Å£¿ÍBC9
+
+
+//#include<stdio.h>
+//int main()
+//{
+//    char ch;
+//    while ((ch = getchar()) != EOF)
+//    {
+//        if (ch >= 'A' && ch <= 'z')
+//            printf("YES");
+//        else
+//            printf("NO");
+//        getchar();
+//    }
+//    return 0;
+//}
+// 
+//Å£¿ÍBC35
+
+//#include<stdio.h>
+//int main()
+//{
+//    int n = 0;
+//    int time = 2;
+//    scanf("%d", &n);
+//    if (n < 12)
+//        time = 2;
+//    else
+//        time += (n / 12) * 4;
+//
+//    printf("%d", time);
+//    return 0;
+//}
+
+//Å£¿ÍBC114
+
